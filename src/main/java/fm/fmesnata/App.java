87232840ebc03053e04758d1e7368bccbd854f18
fmesnata <farid.mesnata.pro@gmail.com>
2021-04-25 15:05:33 +0200

    Change state of cells and refresh display

--- src/main/java/fm/fmesnata/App.java
+++ src/main/java/fm/fmesnata/App.java
@@ -1,61 +1,93 @@
 package fm.fmesnata;
 
 import fm.fmesnata.model.Cell;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class App extends Application {
 
     private static final int CELL_SIZE = 15;
-    public static final int WIDTH = 1280;
-    public static final int HEIGHT = 720;
-    public static final int COLUMN_COUNT = 75;
-    public static final int ROW_COUNT = 40;
-    public static final int SPACE_BETWEEN_CELLS = 1;
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+    private static final int COLUMN_COUNT = 75;
+    private static final int ROW_COUNT = 40;
+    private static final int SPACE_BETWEEN_CELLS = 1;
+    public static final Color DEAD_CELL_COLOR = Color.LIGHTGREY;
+    public static final Color LIVING_CELL_COLOR = Color.FORESTGREEN;
+    private Cell[][] cells;
+    private Timeline timeline;
+    private BorderPane borderPane;
 
     @Override
     public void start(Stage stage) {
-        BorderPane borderPane = new BorderPane();
-        VBox cellsContainer = createCellsContainer();
+        borderPane = new BorderPane();
+        cells = initCells();
+        VBox cellsContainer = createCellsContainer(cells);
+        borderPane.setTop(createToolBar());
         borderPane.setCenter(cellsContainer);
         Scene scene = new Scene(borderPane, WIDTH, HEIGHT);
         stage.setScene(scene);
         stage.show();
+        timeline = createTimeline();
     }
 
     public static void main(String[] args) {
         launch();
     }
 
-    private VBox createCellsContainer() {
+    private Cell[][] initCells() {
+        Cell[][] cc = new Cell[ROW_COUNT][COLUMN_COUNT];
+        for (int row = 0; row < ROW_COUNT; row++) {
+            for (int col = 0; col < COLUMN_COUNT; col++) {
+                Cell cell = new Cell();
+                cell.setAlive(false);
+                cc[row][col] = cell;
+            }
+        }
+        return cc;
+    }
+
+    private VBox createCellsContainer(Cell[][] cells) {
         VBox rows = new VBox();
         rows.setSpacing(1);
-        for (int i = 0; i < ROW_COUNT; i++) {
-            HBox cols = createCellsColumns();
+        for (Cell[] c : cells) {
+            HBox cols = createCellsColumns(c);
             rows.getChildren().addAll(cols);
             rows.setAlignment(Pos.CENTER);
         }
         return rows;
     }
 
-    private HBox createCellsColumns() {
+    private HBox createCellsColumns(Cell[] cols) {
         HBox col = new HBox();
         col.setSpacing(SPACE_BETWEEN_CELLS);
         col.setAlignment(Pos.CENTER);
-        for (int i = 0; i < COLUMN_COUNT; i++) {
-            Cell cell = new Cell();
+        for (Cell cell : cols) {
             cell.setWidth(CELL_SIZE);
             cell.setHeight(CELL_SIZE);
-            cell.setFill(Color.GREY);
+            if (cell.isAlive()) {
+                cell.setFill(LIVING_CELL_COLOR);
+            } else {
+                cell.setFill(DEAD_CELL_COLOR);
+            }
             cell.setOnMouseClicked(changeCellState(cell));
             col.getChildren().add(cell);
         }
@@ -66,11 +98,84 @@ public class App extends Application {
         return event -> {
             if (cell.isAlive()) {
                 cell.setAlive(false);
-                cell.setFill(Color.GREY);
+                cell.setFill(DEAD_CELL_COLOR);
             } else {
                 cell.setAlive(true);
-                cell.setFill(Color.FORESTGREEN);
+                cell.setFill(LIVING_CELL_COLOR);
             }
         };
+    }
+
+    private ToolBar createToolBar() {
+        ToolBar toolBar = new ToolBar();
+        Button startButton = new Button("Start");
+        startButton.setOnMouseClicked(event -> timeline.play());
+        Button stopButton = new Button("Stop");
+        stopButton.setOnMouseClicked(event -> timeline.stop());
+        toolBar.getItems().addAll(startButton, stopButton);
+        return toolBar;
+    }
+
+    private Timeline createTimeline() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), event -> {
+            Cell[][] newCellsState = new Cell[ROW_COUNT][COLUMN_COUNT];
+            for (int row = 0; row < ROW_COUNT; row++) {
+                for (int col = 0; col < COLUMN_COUNT; col++) {
+                    Cell currentCell = cells[row][col];
+                    List<Cell> surroundingCells = new ArrayList<>(8);
+
+                    if (row > 0 && row < ROW_COUNT - 1) {
+                        Cell topCenterCell = cells[row-1][col];
+                        Cell bottomCenterCell = cells[row+1][col];
+                        surroundingCells.addAll(List.of(topCenterCell, bottomCenterCell));
+                        if (col > 0) {
+                            Cell topLeftCell = cells[row-1][col-1];
+                            Cell middleLeftCell = cells[row][col-1];
+                            Cell bottomLeftCell = cells[row+1][col-1];
+                            surroundingCells.addAll(List.of(topLeftCell, middleLeftCell, bottomLeftCell));
+                        }
+                        if (col < COLUMN_COUNT - 1) {
+                            Cell topRightCell = cells[row-1][col+1];
+                            Cell middleRightCell = cells[row][col+1];
+                            Cell bottomRightCell = cells[row+1][col+1];
+                            surroundingCells.addAll(List.of(topRightCell, middleRightCell, bottomRightCell));
+                        }
+                    } else if (row < ROW_COUNT - 1) {
+                        Cell bottomCenterCell = cells[row+1][col];
+                        surroundingCells.addAll(List.of(bottomCenterCell));
+                        if (col > 0) {
+                            Cell middleLeftCell = cells[row][col-1];
+                            Cell bottomLeftCell = cells[row+1][col-1];
+                            surroundingCells.addAll(List.of(middleLeftCell, bottomLeftCell));
+                        }
+                        if (col < COLUMN_COUNT - 1) {
+                            Cell middleRightCell = cells[row][col+1];
+                            Cell bottomRightCell = cells[row+1][col+1];
+                            surroundingCells.addAll(List.of(middleRightCell, bottomRightCell));
+                        }
+                    } else {
+                        Cell topCenterCell = cells[row-1][col];
+                        surroundingCells.addAll(List.of(topCenterCell));
+                        if (col > 0) {
+                            Cell topLeftCell = cells[row-1][col-1];
+                            Cell middleLeftCell = cells[row][col-1];
+                            surroundingCells.addAll(List.of(topLeftCell, middleLeftCell));
+                        }
+                        if (col < COLUMN_COUNT - 1) {
+                            Cell topRightCell = cells[row-1][col+1];
+                            Cell middleRightCell = cells[row][col+1];
+                            surroundingCells.addAll(List.of(topRightCell, middleRightCell));
+                        }
+                    }
+                    Cell newCell = currentCell.nextState(surroundingCells);
+                    newCellsState[row][col] = newCell;
+                }
+            }
+            cells = newCellsState;
+            VBox cellsContainer = createCellsContainer(newCellsState);
+            borderPane.setCenter(cellsContainer);
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        return timeline;
     }
 }
